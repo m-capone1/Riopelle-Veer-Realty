3215fd60b598efdc147c1bf18f0cdd77b2ec7a16
Maddy Capone <maddycapone@gmail.com>
2024-09-30 20:27:02 -0400

    styling of contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,20 @@
 export default function Contact() {
     return (
-        <section>
-            <form className="flex flex-col w-full justify-center items-center shadow-md bg-slate-50">
-                <div className="flex flex-col gap-2 lg:gap-4 justify-center items-center font-bold my-4 text-3xl lg:text-5xl">
+        <section className="mx-4 lg:mx-12">
+            <form className="flex flex-col w-full justify-center items-center shadow-md px-4 pb-4 rounded">
+                <div className="flex flex-col w-full gap-2 lg:gap-4 justify-center items-center font-bold my-4 text-3xl lg:text-5xl">
                     <div className="" >More Questions?</div>
                     <div className="">We're Always Here to Help.</div>
                 </div>
-                <div className="flex flex-row">
-                    <input placeholder="First Name"></input>
-                    <input placeholder="Last Name"></input>
+                <div className="flex flex-row w-full gap-2">
+                    <input placeholder="First Name" className="flex-1 my-2 pl-2 py-2 bg-slate-50 outline-none border-b border-black"></input>
+                    <input placeholder="Last Name" className="flex-1 my-2 pl-2 py-2 bg-slate-50 outline-none border-b border-black"></input>
                 </div>
-                <div>               
-                    <input placeholder="Email"></input>
+                <div className="w-full flex">               
+                    <input placeholder="Email" className="flex-1 my-2 pl-2 py-2 bg-slate-50 outline-none border-b border-black"></input>
                 </div>
-                <div>
-                    <textarea placeholder="Message" className=""></textarea>
+                <div className="w-full flex">
+                    <textarea placeholder="Message" className="flex-1 my-2 pl-2 py-2 h-36 bg-slate-50 outline-none border-b border-black resize-none"></textarea>
                 </div>
             </form>
         </section>
